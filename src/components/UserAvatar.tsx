24a625ac815a53921c0d9579ad072d9d18ec8fae
silqwer <silqwer@gmail.com>
2023-05-28 15:51:57 +0900

    feat: set admin user

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -1,8 +1,8 @@
-import { type User } from 'firebase/auth';
 import React from 'react';
+import { type ShoppyUser } from '../types/User';
 
 interface UserAvatarProps {
-  user: User;
+  user: ShoppyUser;
 }
 
 const UserAvatar: React.FC<UserAvatarProps> = ({ user: { photoURL, displayName } }) => {
@@ -10,7 +10,7 @@ const UserAvatar: React.FC<UserAvatarProps> = ({ user: { photoURL, displayName }
   const name = displayName === null ? undefined : displayName;
 
   return (
-    <div className='flex items-center'>
+    <div className='flex items-center shrink-0'>
       <img src={imgSrc} alt={name} className='w-10 h-10 mr-2 rounded-full' />
       <span className='hidden md:block'>{name}</span>
     </div>
---
+++ src/components/common/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface ButtonProps {
+  text: string;
+  onClick: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
+  return (
+    <button className='px-4 py-2 text-white rounded-sm bg-brand hover:brightness-110' onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
