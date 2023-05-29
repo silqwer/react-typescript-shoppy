1f15a21192845109241864d924b5648a610f164c
silqwer <silqwer@gmail.com>
2023-05-29 13:46:08 +0900

    feat: new product

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 interface ButtonProps {
   text: string;
-  onClick: () => void;
+  onClick?: () => void;
 }
 
 const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
