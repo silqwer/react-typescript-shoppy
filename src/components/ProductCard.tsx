db8efdc9df2be9f7741106ae2fb42aabeb50b159
silqwer <silqwer@gmail.com>
2023-06-02 08:20:07 +0900

    feat: detail product

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { type Product } from '../types/Product';
 
 interface Props {
   product: Product;
 }
 
-const ProductCard: React.FC<Props> = ({ product: { id, image, title, category, price } }) => {
+const ProductCard: React.FC<Props> = ({ product }) => {
+  const { id, image, title, category, price } = product;
+  const navigate = useNavigate();
+
   return (
-    <li className='overflow-hidden rounded-lg shadow-md cursor-pointer'>
+    <li
+      onClick={() => {
+        navigate(`/products/${id}`, {
+          state: product
+        });
+      }}
+      className='overflow-hidden transition-all rounded-lg shadow-md cursor-pointer'
+    >
       <img className='w-full' src={image} alt={title} />
       <div className='flex items-center justify-between px-2 mt-2 text-lg'>
         <h3 className='truncate'>{title}</h3>
